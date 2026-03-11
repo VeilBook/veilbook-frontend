321d890f748e412a7dcb7cd4527659604369f158
devEMEL <61978427+devEMEL@users.noreply.github.com>
2026-03-11 12:07:35 +0100

    footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
                 {/* Copyright */}
                 <div className="pt-8 border-t border-zinc-800/30 text-center">
                     <p className="text-white text-sm">
-                        © 2025 VeilBook. All rights reserved.
+                        © 2026 VeilBook. All rights reserved.
                     </p>
                 </div>
             </div>
@@ -18,68 +18,3 @@ const Footer = () => {
 
 export default Footer;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// "use client";
-
-// import { useFhe } from '@/components/FheProvider';
-// import CWeth from "@/abi/CWeth.json";
-// import { useAccount } from 'wagmi';
-
-
-// export default function Footer() {
-//   const fhe = useFhe();
-//   const { address } = useAccount();
-
-//   const handleEncrypt = async () => {
-//     if (!fhe) return console.log("Still loading...");
-
-//     console.log({fhe});
-
-//     // try this 1
-//     const encryptedValue = await fhe
-//       .createEncryptedInput(CWeth.address, address)
-//       .add64(10)
-//       .encrypt();
-//     console.log({encryptedValue});
-
-//     // try this 2
-//     const encryptedInput = fhe.createEncryptedInput(CWeth.address, address);
-//     encryptedInput.addBool(true)
-//     encryptedInput.add64(10)
-//     const ciphertexts = await encryptedInput.encrypt();
-//     console.log("Encrypted:", ciphertexts);
-
-//     // ciphertexts.handles[0], ciphertexts.inputProof
-//   };
-
-//   return (
-//     <div>
-//       <button 
-//       onClick={handleEncrypt}
-
-//       >Encrypt Value</button>
-//     </div>
-//   );
-// }
